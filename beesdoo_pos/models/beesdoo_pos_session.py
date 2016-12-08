d24e61be7f0dd713d834e4dabadd56d7e7033d71
EliseDup <Elise@wifi-secure1-984.sri.ucl.ac.be>
2016-12-08 09:57:40 +0100

    S026 : Only allow to start session if obening balance has been set

--- beesdoo_pos/models/beesdoo_pos_session.py
+++ beesdoo_pos/models/beesdoo_pos_session.py
@@ -1,15 +1,13 @@
-
-
-
 from openerp import models, fields, api, _
+from openerp.exceptions import UserError
 
 class BeesdooPOSSession(models.Model):
     _inherit = 'pos.session'
-
+    
     def wkf_action_open(self, cr, uid, ids, context=None):
-        print "test"
-        if self.cash_control:
-            if not self.cash_register_balance_start:
-                raise UserError(_('Please set opening balance.'))
-        #return super(pos_session, self).wkf_action_open(cr, uid, ids, context)
-            
+        try:
+            for record in self.browse(cr, uid, ids, context=context):
+                if record.cash_register_balance_start == 0:
+                    raise UserError(_('Please set opening balance.'))
+        except UserError:
+            return super(pos_session, self).wkf_action_open(cr, uid, ids, context=context)
