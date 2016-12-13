8a893a278314b52379d5c9b19ef88e9512d7e18d
EliseDup <Elise@mac-1o0-339.local>
2016-12-13 08:47:49 +0100

    POS session : laisser ouvrir / ferme si le solde n’est pas nul

--- beesdoo_pos/models/beesdoo_pos_session.py
+++ beesdoo_pos/models/beesdoo_pos_session.py
@@ -5,9 +5,13 @@ class BeesdooPOSSession(models.Model):
     _inherit = 'pos.session'
     
     def wkf_action_open(self, cr, uid, ids, context=None):
-        try:
-            for record in self.browse(cr, uid, ids, context=context):
-                if record.cash_register_balance_start == 0:
-                    raise UserError(_('Please set opening balance.'))
-        except UserError:
-            return super(pos_session, self).wkf_action_open(cr, uid, ids, context=context)
+        for record in self.browse(cr, uid, ids, context=context):
+            if record.cash_register_balance_start == 0:
+                raise UserError(_('Please set opening balance.'))
+        return super(BeesdooPOSSession, self).wkf_action_open(cr, uid, ids, context=context)
+    
+    def wkf_action_close(self, cr, uid, ids, context=None):
+        for record in self.browse(cr, uid, ids, context=context):
+            if record.cash_register_balance_end_real == 0:
+                raise UserError(_('Please set closing balance.'))
+        return super(BeesdooPOSSession, self).wkf_action_close(cr, uid, ids, context=context)
