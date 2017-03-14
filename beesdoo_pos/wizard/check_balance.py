7df1bb7ab12d457877c64f993325b0dfe2d1c3ae
EliseDup <Elise@mac-1o0-339.fritz.box>
2017-03-14 20:59:15 +0100

    Test

--- beesdoo_pos/wizard/check_balance.py
+++ beesdoo_pos/wizard/check_balance.py
@@ -6,13 +6,13 @@ class OpeningBalanceWizard(models.TransientModel):
     _name = 'opening.balance.new.wizard'
     
     def _get_opening_balance(self):
-        print "_get_opening_balance", self.env['pos.session'].cash_register_id.balance_start
-        return self.env['pos.session'].cash_register_id.balance_start
+        return self.env['pos.session'].browse(self._context['active_id']).cash_register_id.balance_start
     
-    balance = fields.Float('Opening Balance', default=_get_opening_balance)
-       
+    opening_balance = fields.Float('Opening Balance', default=_get_opening_balance, readonly = True)
+    
     @api.one
     def open_session(self):
-        #self.env['pos.session'].open_session_cb
-       self.env['pos.session'].states="opened"
-       self.env['pos.session'].wkf_action_open
+        print 'open_session'
+        print '#########################'
+        
+        return self.env['pos.session'].browse(self._context['active_id']).open_session
