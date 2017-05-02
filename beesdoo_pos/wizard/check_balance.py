f32711e080e880ec4d8cc43f27b3c903277727c1
EliseDup <Elise@130.104.89.120>
2017-05-02 14:27:02 +0200

    Last changes

--- beesdoo_pos/wizard/check_balance.py
+++ beesdoo_pos/wizard/check_balance.py
@@ -8,15 +8,11 @@ class OpeningBalanceWizard(models.TransientModel):
     def _get_opening_balance(self):
         return self.env['pos.session'].browse(self.env.context.get('active_id')).cash_register_id.balance_start
     
-    opening_balance = fields.Float('Opening Balance', default=_get_opening_balance, readonly = True)
+    opening_balance = fields.Float('Opening Balance', default=_get_opening_balance, readonly=True)
     
     @api.one
     def open_session(self):
-        print 'open_session'
-        print '#########################'
-        
         return self.env['pos.session'].browse(self.env.context.get('active_id')).wkf_action_open()
-    
     
 class ClosingBalanceWizard(models.TransientModel):
     
@@ -25,11 +21,8 @@ class ClosingBalanceWizard(models.TransientModel):
     def _get_closing_balance(self):
         return self.env['pos.session'].browse(self.env.context.get('active_id')).cash_register_id.balance_end_real 
     
-    closing_balance = fields.Float('Opening Balance', default=_get_closing_balance, readonly = True)
+    closing_balance = fields.Float('Closing Balance', default=_get_closing_balance, readonly=True)
     
     @api.one
     def close_session(self):
-        print 'close_session'
-        print '#########################'
-        
         return self.env['pos.session'].browse(self.env.context.get('active_id')).wkf_action_close()
