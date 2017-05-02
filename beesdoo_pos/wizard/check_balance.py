bbbed31accafaab3ac4530980414cd562031a9e3
EliseDup <Elise@130.104.89.120>
2017-05-02 14:18:05 +0200

    Wizard for opening / closing balance

--- beesdoo_pos/wizard/check_balance.py
+++ beesdoo_pos/wizard/check_balance.py
@@ -16,3 +16,20 @@ class OpeningBalanceWizard(models.TransientModel):
         print '#########################'
         
         return self.env['pos.session'].browse(self.env.context.get('active_id')).wkf_action_open()
+    
+    
+class ClosingBalanceWizard(models.TransientModel):
+    
+    _name = 'closing.balance.new.wizard'
+    
+    def _get_closing_balance(self):
+        return self.env['pos.session'].browse(self.env.context.get('active_id')).cash_register_id.balance_end_real 
+    
+    closing_balance = fields.Float('Opening Balance', default=_get_closing_balance, readonly = True)
+    
+    @api.one
+    def close_session(self):
+        print 'close_session'
+        print '#########################'
+        
+        return self.env['pos.session'].browse(self.env.context.get('active_id')).wkf_action_close()
